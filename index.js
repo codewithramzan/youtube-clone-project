972940c1eb00fdb923cf19051e8954cecf8954bb
ramzan khan <animatbyramzan2025>
2025-10-01 10:14:05 -0700

    we fixed when click on menu compress main content

--- index.js
+++ index.js
@@ -31,11 +31,13 @@ const menuButton = document.querySelector('.js-menu-button');
 const sideBar = document.querySelector('.js-left-side-bar');
 
 const sideBarLink = document.querySelectorAll('.js-sidbar-link');
+const mainContent = document.querySelector('.js-video-grid');
 menuButton.addEventListener('click', () => {
-sideBar.classList.toggle('js-left-sidebar');
-sideBarLink.forEach(link => {
-    link.classList.toggle('js-sidebar-link');
-})
+    sideBar.classList.toggle('js-left-sidebar');
+    sideBarLink.forEach(link => {
+        link.classList.toggle('js-sidebar-link');
+    });
+    mainContent.classList.toggle('js-mainContent');
 
 
 });
